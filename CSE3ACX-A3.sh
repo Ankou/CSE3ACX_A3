2ebbe159ffcc17d5f3b48fbcc92fb9817973bae6
Ankou <rickyjfield@hotmail.com>
2024-04-02 21:58:29 +1100

    Shell scripts testing

--- CSE3ACX-A3.sh
+++ CSE3ACX-A3.sh
@@ -6,7 +6,13 @@ logFile=~/$0-$runDate
 echo "Script Starting @ $runDate" > $logFile
 
 # Create VPC
-VPC=$(aws ec2 create-vpc --cidr-block 172.16.0.0/16 --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=a2VPC},{Key=Project,Value="CSE3ACX-A2"}]'  --query Vpc.VpcId --output text)
+VPC=$(aws ec2 create-vpc \
+    --cidr-block 172.16.0.0/16 \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=a2VPC},{Key=Project,Value="CSE3ACX-A2"}]'  \
+    --query Vpc.VpcId --output text)
 
 # Create subnets in the new VPC
-subnet0=$(aws ec2 create-subnet --vpc-id "$VPC" --cidr-block 172.16.0.0/24 --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Subnet0 Public}]' --availability-zone us-east-1a --query Subnet.SubnetId --output text)
+subnet0=$(aws ec2 create-subnet \
+    --vpc-id "$VPC" --cidr-block 172.16.0.0/24 \
+    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Subnet0 Public}]' \
+    --availability-zone us-east-1a --query Subnet.SubnetId --output text)
