46d35851dc0f1871a4dab541b2e73a4d9fcaa60b
Ankou <rickyjfield@hotmail.com>
2024-04-07 20:44:15 +1000

    Shell scripts testing

--- cleanup.sh
+++ cleanup.sh
@@ -27,7 +27,8 @@ echo -e "\e[31mDeleting Elastic Load Balancer\e[0m"
 aws elbv2 delete-load-balancer --load-balancer-arn $elbv2ARN
 
 # Delete Listener
-aws elbv2 delete-listener $listenerARN
+echo -e "\e[31mDeleting Listener\e[0m"
+aws elbv2 delete-listener --load-balancer-arn $listenerARN
 
 # Delete ELB target group
 echo -e "\e[31mDeleting ELB target group\e[0m"
